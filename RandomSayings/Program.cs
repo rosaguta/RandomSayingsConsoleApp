f59ea34ea80c4ac665fc5007423c7d6dc21c4ee5
Rose van Leeuwen | Digital Individuals <rvleeuwen@digitalindividuals.com>
2024-04-17 09:45:37 +0200

    ready for deployment

--- RandomSayings/Program.cs
+++ RandomSayings/Program.cs
@@ -20,7 +20,8 @@ class Program
         int sleepTime = 3000; // Default sleep time
         bool rainbowmode = false;
         bool animate = false;
-
+        int animation_duration = 20;
+        double spread = 0.5;
         for (int i = 0; i < args.Length; i++)
         {
             switch (args[i].ToLower())
@@ -60,8 +61,49 @@ class Program
                 case "-a":
                     animate = true;
                     break;
+                case "--animation_duration":
+                    if (i + 1 < args.Length)
+                    {
+                        if (int.TryParse(args[i + 1], out int time))
+                        {
+                            animation_duration= int.Parse(time.ToString() + "000");
+                            i++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid value provided after --animation_duration flag.");
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No value provided after --animation_duration flag.");
+                        return;
+                    }
+                    break;
+                case "--spread":
+                    if (i + 1 < args.Length)
+                    {
+                        if (double.TryParse(args[i + 1], out double spread_))
+                        {
+                            spread= double.Parse(spread_.ToString() + "");
+                            i++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid value provided after --spread flag.");
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No value provided after --spread flag.");
+                        return;
+                    }
+                    break;
                 default:
-                    Console.WriteLine($"This option is not implemented yet. Please use -q, -r, or -i.");
+                    Console.WriteLine($"No value was provided");
+                    DisplayHelp();
                     return;
             }
         }
@@ -73,24 +115,17 @@ class Program
         {
             if (animate)
             {
-                AnimateLoop(endpoint, sleepTime, _RestClientHelper);
+                AnimateLoop(endpoint, sleepTime, _RestClientHelper,animation_duration, sleepTime, spread);
             }
             RainbowLoop(endpoint, sleepTime, _RestClientHelper);
         }
     }
-
-    static void WriteAt()
+    
+    static void AnimateLoop(string endpoint, int sleepTime, RestClientHelper restClientHelper,int AnimDuration, int waittime, double spread)
     {
-        
-    }
-    static void AnimateLoop(string endpoint, int sleepTime, RestClientHelper restClientHelper)
-    {
-        int spread = 3;
-        int duration = 50;
-        int speed = 24;
         double j = 0.0;    
-        double frequency = 0.7;
         double k = 0.0;
+        double a = 0.0;
 
         Console.CursorVisible = false;
         Console.Clear();
@@ -99,18 +134,21 @@ class Program
         origCol = Console.CursorLeft;
         while(true) 
         {
-            string? response = GetRandomSaying(endpoint, restClientHelper);
-//             string response = @"dit is een mega coole test voor mn kut -a --rainbow functie ;w;
-// dit is een mega coole test voor mn kut -a --rainbow functie ;w;
-// dit is een mega coole test voor mn kut -a --rainbow functie ;w;
-// dit is een mega coole test voor mn kut -a --rainbow functie ;w;
-// dit is een mega coole test voor mn kut -a --rainbow functie ;w; ";
+            // string? response = GetRandomSaying(endpoint, restClientHelper);
+                        string response = @"dit is een mega coole test voor mn kut -a --rainbow functie ;w;
+dit is een mega coole test voor mn kut -a --rainbow functie ;w;
+dit is een mega coole test voor mn kut -a --rainbow functie ;w;
+dit is een mega coole test voor mn kut -a --rainbow functie ;w;
+dit is een mega coole test voor mn kut -a --rainbow functie ;w; ";
             int responselenght = response.Length;
-            for (int i = 0; i < duration; i++)
+            if(response.Contains('\n')){
+                int AmountSlashNInResponse = response.Count(t => t == '\n');
+                a = 1.0 / AmountSlashNInResponse;
+            }
+            for (int i = 0; i < AnimDuration; i++)
             {
                 int columnindex = 0;
                 int rowindex = 0;
-
                 j = k;
                 for (int l = 0; l < responselenght; l++)
                 {
@@ -118,12 +156,12 @@ class Program
                     if (letter == '\n')
                     {
                         rowindex++;
-                        k = k + 0.5;
+                        k = k + a;
                         j = k;
                         columnindex = 0;
                         continue;
                     }
-                    string ColorHex = Rainbow(frequency, j);
+                    string ColorHex = Rainbow(spread, j);
                     // TODO: CHECK FOR TERMINAL SPACE
                     Console.SetCursorPosition(origCol+columnindex, origRow+rowindex);
                     Console.Write(letter.ToString().Pastel(ColorHex));
@@ -136,7 +174,7 @@ class Program
                     j = k;
                 }
             }
-            Thread.Sleep(500);
+            Thread.Sleep(waittime);
             Console.Clear();
             Console.Write("\x1b[3J");
         }
@@ -199,11 +237,13 @@ class Program
     {
         Console.WriteLine("Usage: RandomSayings [OPTIONS]");
         Console.WriteLine("Options:");
-        Console.WriteLine("-q\t\tGet random quotes");
-        Console.WriteLine("-r\t\tGet random rizzes");
-        Console.WriteLine("-i\t\tGet random insults");
-        Console.WriteLine("-t <time>\tSet the sleep time in seconds (default: 3 seconds)");
-        Console.WriteLine("--rainbow\tEnable Rainbow Mode");
-        Console.WriteLine("-h, --help\tDisplay this help message");
+        Console.WriteLine("-q\t\t\tGet random quotes");
+        Console.WriteLine("-r\t\t\tGet random rizzes");
+        Console.WriteLine("-i\t\t\tGet random insults");
+        Console.WriteLine("-t <time>\t\tSet the sleep time in seconds (default: 3 seconds)");
+        Console.WriteLine("--rainbow\t\tEnable Rainbow Mode");
+        Console.WriteLine("--animation_duration\tSet the animation duration by X value. This value isn't related to time (default: 20");
+        Console.WriteLine("--spread \t\tSet the spread of the rainbow. Lower = more spread (default: 0.5)");
+        Console.WriteLine("-h, --help\t\tDisplay this help message");
     }
 }
