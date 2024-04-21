e8b35f4b9d17b05b742a34cf9ede05834e74b5ff
Rose <rvleeuwen@DigitalIndividuals.com>
2024-04-21 18:18:28 +0200

    extra help thingy

--- RandomSayings/Program.cs
+++ RandomSayings/Program.cs
@@ -43,6 +43,7 @@ class Program
         Console.WriteLine("-i\t\t\t\tGet random insults");
         Console.WriteLine("-t <time>\t\t\tSet the sleep time in seconds (default: 3 seconds)");
         Console.WriteLine("--rainbow\t\t\tEnable Rainbow Mode");
+        Console.WriteLine("-a \t\t\t\tEnable animation. This needs to be activated with the \x1b[1m--rainbow \x1b[0mparameter (RandomQuotes --rainbow -a)");
         Console.WriteLine("--animation_duration <value> \tSet the animation duration by X value. This value isn't related to time (default: 20)");
         Console.WriteLine("--spread <value> \t\tSet the spread of the rainbow. Lower = more spread (default: 0.5)");
         Console.WriteLine("-h, --help\t\t\tDisplay this help message");
